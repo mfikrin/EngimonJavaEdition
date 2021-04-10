9e0cc5a46e38bb2cfcea7de4160761a97d63fa2c
Randy Zakya <impper1@gmail.com>
2021-04-10 12:45:34 +0700

    skillitem learn

--- src/Entity/Engimon/Engimon.java
+++ src/Entity/Engimon/Engimon.java
@@ -11,6 +11,7 @@ public class Engimon implements Element,Clone<Engimon>{
     static final int ratio_exp = 100;
     static final int max_skills = 4;
     private String name = getClass().getSimpleName();
+    private String species = getClass().getSimpleName();
     private int level = 1;
     private int experience = 0;
     private int cumulative_experience = 0;
@@ -25,6 +26,8 @@ public class Engimon implements Element,Clone<Engimon>{
     }
 
     public Engimon(Engimon other){
+        this.name = other.name;
+        this.species = other.species;
         this.level = other.level;
         this.experience = other.experience;
         this.cumulative_experience = other.cumulative_experience;
@@ -51,14 +54,20 @@ public class Engimon implements Element,Clone<Engimon>{
     public int get_skill_size(){return list_skill.size();}
     public ArrayList<ArrayList<String>> get_parent(){return parent;}
     public int get_max_exp(){return current_max_exp;}
-    public String get_species(){return getClass().getSimpleName();}
+    public String get_species(){return species;}
 
     public void set_name(String name){this.name = name;}
     public void set_level(int level){this.level = level;}
     public void set_exp(int experience){this.experience = experience;}
     public void set_cexp(int cumulative_experience){this.cumulative_experience = cumulative_experience;}
     public void set_live(int live){this.live = live;}
-    public void add_skill(Skill new_skill){list_skill.add(new_skill);}
+    public boolean add_skill(Skill new_skill){
+        if (list_skill.size() < max_skills){
+            list_skill.add(new_skill);
+            return true;
+        }
+        return false;
+    }
     //bagian gaboleh lebih dari 4 skill kayaknya diatur di player atau gui2an
     public void set_skill(Skill new_skill, int i){list_skill.set(i,new_skill);}
     public void add_parent(ArrayList<String> parent_){parent.add(parent_);}
--- src/Entity/SkillItem.java
+++ src/Entity/SkillItem.java
@@ -5,26 +5,24 @@ import src.Entity.Skill.Skill;
 
 public class SkillItem {
     private Skill contained_skill;
-    private int number;
+    private int quantity;
 
     public SkillItem(Skill skill){
-        contained_skill = skill;
-        number = 1;
+        contained_skill = skill.clone();
+        quantity = 0;
     }
-    public void incr_n(){
-        number++;
+    public void incr_quantity(){
+        quantity++;
     }
-    public void decr_n(){
-        number--;
-    }
-    public int get_number(){
-        return number;
-    }
-    public Skill get_skill(){
-        return contained_skill;
-    }
-    public void learn(Engimon learner){
-        
+    public boolean learn(Engimon learner){
+        if (quantity > 0){
+            if (learner.add_skill(contained_skill.clone())){
+                quantity--;
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
     //untuk lebih dari 4 skill kayaknya diatur di player atau gui2an
 }
